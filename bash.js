83a6986a6c286f632466da46c39141b5261b165d
Kevin Cho <kevinhcho27@gmail.com>
2018-11-06 14:27:28 -0500

    finished up to bonus

--- bash.js
+++ bash.js
@@ -1,5 +1,17 @@
-const pwd = require('./pwd')
-
+process.stdout.write('prompt > ')
+process.stdin.on('data', (data) => {
+    const pwd = require('./pwd')
+    const ls = require('./ls')
+    const cmd = data.toString().trim()
+    if (cmd === 'pwd') {
+        pwd()
+    } else if (cmd === 'ls') {
+        ls()
+    } else {
+        process.stdout.write('You typed: ' + cmd)
+    }
+    process.stdout.write('\nprompt > ')
+})
 
 // process.stdout.write('prompt > ')
 // process.stdin.on('data', (data) => {
--- pwd.js
+++ pwd.js
@@ -1,10 +1,16 @@
-module.export = process.stdout.write('prompt > ')
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim()
-    if (cmd === 'pwd') {
-        process.stdout.write(__dirname)
-    } else {
-        process.stdout.write('You typed: ' + cmd)
-    }
+module.exports = function() {
+    process.stdout.write(__dirname)
     process.stdout.write('\nprompt > ')
-})
+}
+
+
+// module.export = process.stdout.write('prompt > ')
+// process.stdin.on('data', (data) => {
+//     const cmd = data.toString().trim()
+//     if (cmd === 'pwd') {
+//         process.stdout.write(__dirname)
+//     } else {
+//         process.stdout.write('You typed: ' + cmd)
+//     }
+//     process.stdout.write('\nprompt > ')
+// })
